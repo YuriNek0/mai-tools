70a301564ea05a6a3e1b74f3ba65348f9eb4a640
Ming-yuen Jien <1978764+myjian@users.noreply.github.com>
2026-02-12 05:04:18 +0900

    Convert class components to functions (part 1) (#137)

--- src/classic-layout/components/NextRankInfo.tsx
+++ src/classic-layout/components/NextRankInfo.tsx
@@ -1,39 +1,32 @@
-import React from 'react';
+import {useMemo} from 'react';
 
 interface NextRankInfoProps {
-  nextRank?: {title: string, diff: number};
+  nextRank?: {title: string; diff: number};
   showTitle: boolean;
 }
 
-export class NextRankInfo extends React.PureComponent<NextRankInfoProps> {
-  state = {showTitle: false};
-  render() {
-    const {nextRank, showTitle} = this.props;
-    const nextRankTitle = (showTitle && nextRank) ? nextRank.title : "";
-    const nextRankDiff = this.getNextRankDiff();
-    return (
-      <tr className="nextRank">
-        <th className="noRightBorder" colSpan={4}>NEXT RANK</th>
-        <td className="noLeftBorder" colSpan={2}>
-          {nextRankTitle && <span className="nextRankTitle">{nextRankTitle}</span>}
-          {nextRankDiff && <span className="nextRankDiff">{nextRankDiff}</span>}
-        </td>
-      </tr>
-    );
-  }
-
-  private getNextRankDiff() {
-    const {nextRank} = this.props;
+export const NextRankInfo = ({nextRank, showTitle}: NextRankInfoProps) => {
+  const nextRankTitle = showTitle && nextRank ? nextRank.title : '';
+  const nextRankDiff = useMemo(() => {
     if (!nextRank) {
-      return "—————";
+      return '—————';
     }
     const {diff} = nextRank;
-    if (typeof diff === "number") {
-      if (Math.round(diff) !== diff) {
-        return diff.toFixed(4) + "%";
-      }
-      return diff.toLocaleString("en");
-    }
-    return diff;
-  }
-}
+    return typeof diff !== 'number'
+      ? diff
+      : Math.round(diff) === diff
+        ? diff.toLocaleString('en')
+        : diff.toFixed(4) + '%';
+  }, [nextRank]);
+  return (
+    <tr className="nextRank">
+      <th className="noRightBorder" colSpan={4}>
+        NEXT RANK
+      </th>
+      <td className="noLeftBorder" colSpan={2}>
+        {nextRankTitle && <span className="nextRankTitle">{nextRankTitle}</span>}
+        {nextRankDiff && <span className="nextRankDiff">{nextRankDiff}</span>}
+      </td>
+    </tr>
+  );
+};
